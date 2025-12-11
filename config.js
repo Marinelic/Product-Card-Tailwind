792e15784a2742801658dbfcdc7879a829763681
Marinela Rajković <marinela.rajkovic29@gmail.com>
2025-12-11 13:04:34 +0100

    Bg image on mobile and desktop

--- config.js
+++ config.js
@@ -4,12 +4,18 @@ tailwind.config = {
             colors: {
                 my_strawberry: '#ede1e6',
                 strong_pink: '#e39199',
-                mid_pink: '#fadef4ff'
+                mid_pink: '#fadef4ff',
+                strawberrie_pink: '#d45b63'
             },
 
             fontFamily: {
                 sans: ['Josefin Sans', 'sans-serif'],
                 title: ['Playfair Display SC', 'serif']
+            },
+
+            backgroundImage: {
+                mobile: "url('mobile.jpg')",
+                desktop: "url('desktop.jpg')"
             }
         }
     }
