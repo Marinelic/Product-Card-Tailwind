afa5eccb712b5a07c07b417ede05dc4f9472ba3c
Marinela Rajković <marinela.rajkovic29@gmail.com>
2025-12-11 11:53:06 +0100

    Stying  the Cart

--- config.js
+++ config.js
@@ -2,7 +2,14 @@ tailwind.config = {
     theme: {
         extend: {
             colors: {
-                my_strawberry: 'strawberry'
+                my_strawberry: '#ede1e6',
+                strong_pink: '#e39199',
+                mid_pink: '#fadef4ff'
+            },
+
+            fontFamily: {
+                sans: ['Josefin Sans', 'sans-serif'],
+                title: ['Playfair Display SC', 'serif']
             }
         }
     }
